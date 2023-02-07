dfd55ab8e0ec5c1c39a11100ff0fcf572cad8956
PinoMacri <pinonegozio1@outlook.it>
2023-02-07 18:58:03 +0100

    Esercizio Consegnato

--- index.php
+++ index.php
@@ -46,22 +46,35 @@ $hotels = [
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Php-Hotel</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
 
-<div>
- <?php foreach($hotels as $hotel) : ?>
-    <ul>
-     <li>
-    <?php 
-        foreach($hotel as $chiave => $valore ) {
-            echo $chiave.": ".$valore."<br />";  
-        }
-    ?>
-     </li>
-    </ul>
- <?php endforeach ?>
-</div> 
+<section class="container mt-5">
+<h1 class="text-white">Hotels</h1>
+ <table class="table">
+  <thead>
+    <tr class="text-white">
+    <?php foreach ($hotels[0] as $key => $valore) : ?>
+        <th scope="col"><?= ucfirst($key) ?></th>
+    <?php endforeach; ?>
+    </tr>
+  </thead>
+  <tbody>
+  <?php foreach($hotels as $hotel) : ?> 
+    <tr class="text-white" > 
+        <td scope="row"><?php echo $hotel["name"] ?></td>
+        <td scope="row"><?php echo $hotel["description"] ?></td>
+        <td scope="row"><?php echo $hotel["parking"] === true ? '<i class="fa-solid fa-circle-check"></i>' : '<i class="fa-solid fa-circle-xmark"></i>' ?></td>
+        <td scope="row"><?php echo $hotel["vote"] ?></td>
+        <td scope="row"><?php echo $hotel["distance_to_center"] ?></td>
+    </tr>
+    <?php endforeach ?>
+  </tbody>
+ </table>
+</section>
 
 </body>
 </html>
